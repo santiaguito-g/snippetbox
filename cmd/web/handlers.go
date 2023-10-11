24764c8533bf412531907e37d4a1be6523a501ae
Santi Gonzalez <santi.gnzlz@proton.me>
2023-10-10 22:46:53 -0300

    refactor: project structure

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -24,27 +22,15 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		const StatusMethodNotAllowed int = 405
-
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", 405)
 		return
 	}
-	w.Write([]byte("Create a new snippet.."))
-}
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	w.Write([]byte("Create a new snippet..."))
 }
