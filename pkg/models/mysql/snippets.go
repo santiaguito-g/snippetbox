659fa7227260d8e0c237ee2cefb9e90aa3efcb8a
Santi Gonzalez <santi.gnzlz@proton.me>
2023-10-11 23:37:19 -0300

    add SQL statements

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -10,8 +10,21 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-func (m *SnippetModel) Insert(title, content, created, expires string) (int, error) {
-	return 0, nil
+func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
